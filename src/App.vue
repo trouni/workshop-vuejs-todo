9fdb85cf5e4534b969e9eb873417ce165b1a2784
Trouni Tiet <trouni@gmail.com>
2021-04-05 15:56:11 +0900

    Set blank state for workshop

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,16 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" height="100" />
     <h1>Task Manager</h1>
-    <TasksList />
+    <p>Let's build our first Vue.js app!</p>
   </div>
 </template>
 
 <script>
-import TasksList from "./components/TasksList.vue";
-
 export default {
   name: "App",
-
-  components: {
-    TasksList,
-  },
 };
 </script>
 
 <style lang="scss">
-@import "./assets/style.scss";
+// @import "./assets/style.scss";
 </style>
